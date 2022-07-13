7e20d02eb96d985a8c600b24f9fb4a7639ba5dbe
VSemikoz <slava.semik@yandex.ru>
2022-07-13 17:36:56 +0300

    Add sinc store

---
+++ lib/src/services/local/base_sink_storage.dart
@@ -0,0 +1,54 @@
+import 'package:matrix_sdk/matrix_sdk.dart';
+
+import '../../model/context.dart';
+
+abstract class BaseSinkStorage {
+  Stream<MyUser> myUserStorageSink(String userId);
+
+  bool isReady();
+
+  Future<void> setUserDelta(MyUser user);
+
+  Future<void> setRoom(Room room);
+
+  Future<List<String?>?> getRoomIds();
+
+  Future<Iterable<Room>> getRoomsByIds(
+    Iterable<RoomId>? roomIds, {
+    Context? context,
+    required int timelineLimit,
+    Iterable<UserId>? memberIds,
+  });
+
+  Future<Iterable<Room>> getRooms({
+    Context? context,
+    required int limit,
+    required int offset,
+    required int timelineLimit,
+    Iterable<UserId>? memberIds,
+  });
+
+  Future<Messages> getMessages(
+    RoomId roomId, {
+    int count = 20,
+    DateTime? fromTime,
+    Iterable<UserId>? memberIds,
+  });
+
+  Future<Iterable<Member>> getMembers(
+    RoomId roomId, {
+    int count = 20,
+    DateTime? fromTime,
+  });
+
+  Future<Room?> getRoom(
+    RoomId id, {
+    int timelineLimit = 15,
+    required Context context,
+    required Iterable<UserId> memberIds,
+  });
+
+  Future<String?> getToken(String id);
+
+  Future<void> close();
+}
